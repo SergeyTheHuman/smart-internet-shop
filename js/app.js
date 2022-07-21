378bcf31985c7bb7c72b1b3ac0b70a31204ac3c1
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-07-21 12:01:48 +0300

    created contacts page + adaptive + yandex map

--- js/app.js
+++ js/app.js
@@ -43,6 +43,11 @@ const $compareTable = document.querySelector('.compare__table')
 const $products = document.querySelectorAll('.product-card')
 const $productOne = document.querySelectorAll('.product-one')
 
+// contacts page
+const $buttonContactsPage = document.querySelector('.contacts-form__button')
+const $contactsFormFirstColumn = document.querySelector('[data-contacts-col="first"]')
+const $contactsFormLastColumn = document.querySelector('[data-contacts-col="last"]')
+
 if (document.querySelector('.product-tabs')) {
 	const productTabs = new Tabs('.product-tabs')
 }
@@ -169,6 +174,15 @@ window.addEventListener('resize', (e) => {
 			$compareControls.appendChild($compareCheckbox)
 		}
 	}
+
+	// Button on contacts page
+	if ($buttonContactsPage && $contactsFormFirstColumn && $contactsFormLastColumn) {
+		if (window.innerWidth < 678) {
+			$contactsFormLastColumn.appendChild($buttonContactsPage)
+		} else {
+			$contactsFormFirstColumn.appendChild($buttonContactsPage)
+		}
+	}
 })
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -193,6 +207,15 @@ document.addEventListener('DOMContentLoaded', () => {
 		const accordion1 = new AccISV('.footer__accordion--1')
 		const accordion2 = new AccISV('.footer__accordion--2')
 		const accordion3 = new AccISV('.footer__accordion--3')
+	}
+
+	// Button on contacts page
+	if ($buttonContactsPage && $contactsFormFirstColumn && $contactsFormLastColumn) {
+		if (window.innerWidth < 678) {
+			$contactsFormLastColumn.appendChild($buttonContactsPage)
+		} else {
+			$contactsFormFirstColumn.appendChild($buttonContactsPage)
+		}
 	}
 })
 
@@ -285,7 +308,7 @@ document.addEventListener('click', (e) => {
 	}
 })
 
-if ($checkboxOnlyDifferent) {
+if ($checkboxOnlyDifferent && $compareTable) {
 	$checkboxOnlyDifferent.addEventListener('change', () => {
 		if ($checkboxOnlyDifferent.checked) {
 			$compareTable.classList.add('only-different')
@@ -331,7 +354,6 @@ if ($products) {
 }
 
 if ($productOne) {
-	console.log($productOne)
 	$productOne.forEach((el) => {
 		//Prices vars
 		const $priceBlock = el.querySelector('[data-price]')
@@ -356,4 +378,104 @@ if ($productOne) {
 			$newPrice.textContent = `${mainprice} ₽`
 		}
 	})
+}
+
+if (document.getElementById('contacts__map')) {
+	function initYandexMap() {
+		let map = new ymaps.Map('contacts__map', {
+			center: [59.92459416911864, 30.378338001382485],
+			zoom: 12,
+		})
+
+		// Создание геообъекта с типом точка (метка) без текста.
+		const geoPoint1 = new ymaps.Placemark(
+			[59.944126447716435, 30.377038384421212], // куда вставить метку
+			{
+				balloonContent: `
+					<div class="myballoon"> 
+						<h5 class="myballoon__name">Магазин №3</h5>
+						<div class="myballoon__address">Невский 140</div>
+					</div>
+				`,
+			},
+			{
+				iconLayout: 'default#image', // значение 'default#image' обязательно
+				iconImageHref: '../images/content-icons/map-mark.svg', // путь к изображению
+				iconImageSize: [50, 50], // размер изображения
+				iconImageOffset: [0, 0], // сдвиг значка относительно точки привязки (задается, если "ножка" значка не находится в левом верхнем углу изображения)
+			}
+		)
+
+		// Создание геообъекта с типом точка (метка) c баллуном текста.
+		const geoPoint2 = new ymaps.Placemark(
+			[59.92532662191352, 30.402799747598326],
+			{
+				balloonContent: `
+					<div class="myballoon"> 
+						<h5 class="myballoon__name">Магазин №1</h5>
+						<div class="myballoon__address">Невский 140</div>
+					</div>
+				`,
+			},
+			{
+				iconLayout: 'default#image',
+				iconImageHref: '../images/content-icons/map-mark.svg',
+				iconImageSize: [40, 56],
+				iconImageOffset: [0, 0],
+			}
+		)
+
+		const geoPoint3 = new ymaps.Placemark(
+			[59.920994875490514, 30.33618297670637],
+			{
+				balloonContent: `
+					<div class="myballoon"> 
+						<h5 class="myballoon__name">Магазин №2</h5>
+						<div class="myballoon__address">Невский 140</div>
+					</div>
+				`,
+			},
+			{
+				iconLayout: 'default#image',
+				iconImageHref: '../images/content-icons/map-mark.svg',
+				iconImageSize: [40, 56],
+				iconImageOffset: [0, 0],
+			}
+		)
+
+		const geoPoint4 = new ymaps.Placemark(
+			[59.936729938080944, 30.37284483731999],
+			{
+				balloonContent: `
+					<div class="myballoon"> 
+						<h5 class="myballoon__name">Магазин №4</h5>
+						<div class="myballoon__address">Невский 140</div>
+					</div>
+				`,
+			},
+			{
+				iconLayout: 'default#image',
+				iconImageHref: '../images/content-icons/map-mark.svg',
+				iconImageSize: [40, 56],
+				iconImageOffset: [0, 0],
+			}
+		)
+
+		// Размещение геообъекта на карте.
+		map.geoObjects.add(geoPoint1)
+		map.geoObjects.add(geoPoint2)
+		map.geoObjects.add(geoPoint3)
+		map.geoObjects.add(geoPoint4)
+
+		map.controls.remove('geolocationControl') // удаляем геолокацию
+		map.controls.remove('searchControl') // удаляем поиск
+		map.controls.remove('trafficControl') // удаляем контроль трафика
+		map.controls.remove('typeSelector') // удаляем тип
+		map.controls.remove('fullscreenControl') // удаляем кнопку перехода в полноэкранный режим
+		map.controls.remove('rulerControl') // удаляем контрол правил
+		// map.controls.remove('zoomControl') // удаляем контрол зуммирования
+		// map.behaviors.disable(['scrollZoom']) // отключаем скролл карты (опционально)
+	}
+
+	ymaps.ready(initYandexMap)
 }
